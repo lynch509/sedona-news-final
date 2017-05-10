3550f223cae24dfe459008a3a97960535f64093c
lynch509 <kellylynch@Kellys-MacBook-Pro-9.local>
2017-05-10 13:33:25 -0500

    upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/*! jQuery v1.7.2 jquery.com | jquery.org/license */
+
+
+$(document).ready(function(){
+
+	$("#smoke-quote-top").delay(100).fadeIn("90000000000000000000000000000000000000000000000000000000000000000000000000");
+
+});
+
+
+
+
+
+
+
+
+
